9caa94c0ae8dd0bbc4b99dbe4079cba67d8aa5f3
AhmedRaheem <amed-raheem@outlokk.com>
2025-02-24 00:28:28 +0200

    Fixs

--- HijJobRequests/Program.cs
+++ HijJobRequests/Program.cs
@@ -9,22 +9,15 @@ using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add database context with SQL Server connection
 builder.Services.AddDbContext<DbIthraaContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection_101")));
 
-// Add controllers and apply global filters
 builder.Services.AddControllers(options => { options.Filters.Add<ApiResponseFilter>(); });
 builder.Services.AddHttpContextAccessor();
-
-// Register services for the current user
 builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
-
-// Add Swagger for API documentation
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Add authentication using JWT
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -45,59 +38,20 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
-// Add CORS policy to allow all origins, methods, and headers
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll",
-        builder =>
-        {
-            builder.AllowAnyOrigin()
-                   .AllowAnyMethod()
-                   .AllowAnyHeader();
-        });
-});
-
 var app = builder.Build();
 
-// Configure the app for development environment
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-// Enable CORS
-app.UseCors("AllowAll");
-
-// Serve static files (e.g., React app)
+app.UseCors(b => b.WithOrigins("https://sadatest.sada-com.com", "http://localhost:3000", "http://localhost:5287").AllowAnyHeader().AllowAnyMethod());
 app.UseStaticFiles();
 
-// Redirect all non-API, non-file requests to index.html for React app routing
-//app.Use(async (context, next) =>
-//{
-//    // Check if the request is for an API or if it's a request for a file (like .css, .js, etc.)
-//    if (!context.Request.Path.Value.StartsWith("/api") &&
-//        !System.IO.Path.HasExtension(context.Request.Path.Value))
-//    {
-//        // Redirect to index.html for all other requests (React frontend)
-//        context.Response.Redirect("/index.html");
-//        return;
-//    }
-//    await next();
-//});
-
-// Enable HTTPS redirection
-//app.UseHttpsRedirection();
-
-// Set up authentication and authorization middleware
+// app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
-
-// Map controllers for API endpoints
 app.MapControllers();
 app.MapFallbackToFile("index.html");
-// Apply custom error handling middleware (optional)
 app.UseMiddleware<ErrorHandlerMiddleware>();
-
-// Start the app
 app.Run();
