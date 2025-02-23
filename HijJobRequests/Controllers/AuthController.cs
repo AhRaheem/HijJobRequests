639d255c48e3cabfee5bb5fa1017aa6cc6016e21
AhmedRaheem <amed-raheem@outlokk.com>
2025-02-23 02:07:37 +0200

    Fixs

--- HijJobRequests/Controllers/AuthController.cs
+++ HijJobRequests/Controllers/AuthController.cs
@@ -2,6 +2,7 @@ using System.Data.Entity;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using HijJobRequests.Dtos;
 using HijJobRequests.Models;
 using HijJobRequests.Vars;
 using Microsoft.AspNetCore.Http;
@@ -24,16 +25,16 @@ namespace HijJobRequests.Controllers
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<string>> VerifyLogin(string Identityfier, string Password, string Company)
+        public async Task<ActionResult<string>> VerifyLogin(LoginDto model)
         {
-            return Ok((await _context.AppUsers.FirstOrDefaultAsync(x => x.FEmail.Equals(Identityfier) || x.FJawNo.Equals(Identityfier) || x.FIdNo.Equals(Identityfier) && x.FUserPass.Equals(Password))) is not null ? SetAppUserOtp(Identityfier) : string.Empty);
+            return Ok((await _context.AppUsers.FirstOrDefaultAsync(x => x.FEmail.Equals(model.Identityfier) || x.FJawNo.Equals(model.Identityfier) || x.FIdNo.Equals(model.Identityfier) && x.FUserPass.Equals(model.Password))) is not null ? SetAppUserOtp(model.Identityfier) : string.Empty);
         }
 
         [HttpPost("VerifyOtp")]
-        public async Task<ActionResult<string>> VerifyOtp(string Identityfier, string Otp, string Company)
+        public async Task<ActionResult<string>> VerifyOtp(LoginDto model)
         {
             SharedVars.AppUsersOtps.RemoveWhere(x => x.Item3 < DateTime.UtcNow);
-            return Ok(SharedVars.AppUsersOtps.Any(x => x.Item1.Equals(Identityfier) && x.Item2.Equals(Otp)) ? await GenerateToken(Identityfier, Company) : string.Empty);
+            return Ok(SharedVars.AppUsersOtps.Any(x => x.Item1.Equals(model.Identityfier) && x.Item2.Equals(model.Otp)) ? await GenerateToken(model.Identityfier, model.Company) : string.Empty);
         }
 
         // [HttpGet("GetMenuItems/{UserId}")]
---
+++ HijJobRequests/Dtos/LoginDto.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HijJobRequests.Dtos;
+
+public class LoginDto
+{
+    public string Identityfier { get; set; }
+    public string Password { get; set; }
+    public string Company { get; set; }
+    public string Database { get; set; }
+    public string ApiKey { get; set; }
+    public string Otp { get; set; }
+}
